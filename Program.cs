f70a95de87520ea53dc77d41f7634e69d579abc7
neithel <neithel@protonmail.com>
2018-06-16 18:58:12 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SIEMA");
+            Console.ReadLine();
+        }
+    }
+}
